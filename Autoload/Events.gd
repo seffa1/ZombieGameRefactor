b12ec73f9acb1ab992d007062fac6487c1fb6429
sam effa <sameffa1@gmail.com>
2023-07-24 07:30:51 -0400

    bullet and object registry

--- Autoload/Events.gd
+++ Autoload/Events.gd
@@ -9,3 +9,4 @@ The game initializer instead connects things up to this signal.
 signal player_log(message: String)
 signal update_interactable_log(message: String)
 
+signal damaged(target, damage, shooter)
---
+++ Autoload/ObjectRegistry.gd
@@ -0,0 +1,15 @@
+extends Node
+
+"""
+Creates, maintains, and organizes spawned special effects or projectiles;
+objects that should be untied from their spawners' lifespan when freed.
+"""
+
+@onready var _effects := $Effects
+@onready var _projectiles := $Projectiles
+
+func register_effect(effect: Node) -> void:
+	_effects.add_child(effect)
+
+func register_projectile(projectile: Node) -> void:
+	_projectiles.add_child(projectile)
