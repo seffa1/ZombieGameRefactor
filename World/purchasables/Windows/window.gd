e942e16477ec8b3924441e4e67487ca2b1f75827
sam effa <sameffa1@gmail.com>
2023-07-31 19:59:38 -0400

    windows

--- World/purchasables/Windows/window.gd
+++ World/purchasables/Windows/window.gd
@@ -2,6 +2,8 @@ extends "res://World/purchasables/purchasable.gd"
 
 @export var window_repair_money_reward: int = 10
 @onready var health_component: Node2D = $HealthComponent
+@onready var animation_player: AnimationPlayer = $AnimationPlaceholder
+@onready var collision_environment: CollisionShape2D = $StaticBody2D/CollisionShape1
 
 func give_item(player: CharacterBody2D) -> void:
 	"""
@@ -11,6 +13,7 @@ func give_item(player: CharacterBody2D) -> void:
 	Events.emit_signal("give_player_money", window_repair_money_reward)
 	
 	health_component.health += 1  # Window gets 1 health for each repair
+	update_animation_frame(health_component.health)
 	return
 
 func get_interactable_message() -> String:
@@ -18,3 +21,21 @@ func get_interactable_message() -> String:
 		return "Repair " + purchasable_name
 	else:
 		return ""
+
+func update_animation_frame(health: int) -> void:
+	"""
+	Frame here is really seconds. But The animation should have 1 second
+	step size to effectively make 1sec/frame.
+	"""
+	var MAX_FRAMES_MAX_HEALTH = 7
+	var frame = MAX_FRAMES_MAX_HEALTH - health
+	print("animation update")
+	animation_player.play("window_break")
+	animation_player.seek(float(frame), true)
+	animation_player.pause()
+	
+	# set window collisions at the end of the current frame
+	if health_component.health <= 0:
+		collision_environment.set_deferred("disabled", true)
+	else:
+		collision_environment.set_deferred("disabled", false)
--- World/purchasables/Windows/window_hurtbox.gd
+++ World/purchasables/Windows/window_hurtbox.gd
@@ -3,12 +3,17 @@ extends Area2D
 
 @export var health_component: Node2D
 @export var damage_timer: Timer
-@export var damage_interval: int
+@export var damage_interval: float
 
 func _on_area_entered(area: Area2D):
 	"""
 	The area here is the enemy's hitbox.
 	"""
+	
 	if damage_timer.is_stopped():
 		damage_timer.start(damage_interval)
 		health_component.health -= 1  # window takes 1 damage for each zombie hit
+		owner.update_animation_frame(health_component.health)
+		
+		
+
