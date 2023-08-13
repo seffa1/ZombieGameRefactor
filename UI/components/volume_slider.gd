275640df7fd2753546d59936b068a69675797279
sam effa <sameffa1@gmail.com>
2023-08-13 18:26:08 -0400

    fixed volume slider range

--- UI/components/volume_slider.gd
+++ UI/components/volume_slider.gd
@@ -13,6 +13,11 @@ func _ready() -> void:
 		AudioServer.get_bus_volume_db(bus_index)
 	)
 func _on_value_changed(value: float) -> void:
+	print("------------")
+	print(AudioServer.get_bus_volume_db(bus_index))
+	print(linear_to_db(value))
+	
+	
 	AudioServer.set_bus_volume_db(
 		bus_index,
 		linear_to_db(value)
