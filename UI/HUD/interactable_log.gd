ee1c307938e6cf2bcf9b74b3583ccd43e871678c
sam effa <sameffa1@gmail.com>
2023-08-27 20:04:33 -0400

    interactable log bug and more labs

--- UI/HUD/interactable_log.gd
+++ UI/HUD/interactable_log.gd
@@ -5,6 +5,7 @@ extends Control
 func _ready() -> void:
 	Events.update_interactable_log.connect(show_message)
 	log_message.text = ""
+	show()
 
 func show_message(message: String):
 	log_message.text = message
