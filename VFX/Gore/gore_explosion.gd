198c84d9d9d94c46af182bcd4e993aee7b8aab49
Sam <94399429+seffa1@users.noreply.github.com>
2023-09-08 11:33:47 -0400

    replaced explosion death particle vfx with the rigid body vfx

--- VFX/Gore/gore_explosion.gd
+++ VFX/Gore/gore_explosion.gd
@@ -33,11 +33,22 @@ func bullet_impact(velocity: Vector2):
 	impact_audio.play_random()
 
 func explosion_death(zombie_position: Vector2, explosion_position: Vector2):
+	# Spawn gore in the direction of the velocity (the -90 is because the vfx spawner defaults to a vector.DOWN
+	var angle = (zombie_position - explosion_position).angle()
+	
+	for body_part_spawner in body_parts.get_children():
+		body_part_spawner.spawn_item(angle - deg_to_rad(90))
+
+func play_splatter():
+	splatter_audio.play_random()
+
+func spawn_body_part_particle_system(zombie_position: Vector2, explosion_position: Vector2):
+	"""
+	This was a test but is not going to be used. Particles dont make sense for body parts
+	since they need to be rigid bodies to be interactable
+	"""
 	var direction_vector = (zombie_position - explosion_position).normalized()
 	var body_part_explosion_vfx = body_part_explosion.instantiate()
 	body_part_explosion_vfx.global_position = zombie_position
 	body_part_explosion_vfx.rotation = direction_vector.angle()
 	ObjectRegistry.register_effect(body_part_explosion_vfx)
-
-func play_splatter():
-	splatter_audio.play_random()
