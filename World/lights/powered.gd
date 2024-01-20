e94b99735f40c278996ddaf5de06bf57f846f2b2
sam effa <sameffa1@gmail.com>
2024-01-19 20:01:59 -0500

    powered lights

---
+++ World/lights/powered.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var light = $".."
+@export var default_is_on: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Events.power_activated.connect(_on_power_on)
+	light.enabled = default_is_on
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_power_on():
+	print("POWER On")
+	light.enabled = true
---
+++ World/purchasables/perks/powered_light.gd
@@ -0,0 +1,3 @@
+extends PointLight2D
+
+
