b1f5ad981bbf6aa31f87c23009c11f5626df41d6
sam effa <sameffa1@gmail.com>
2024-01-25 19:57:18 -0500

    zombie bomber start

---
+++ Enemies/Bomber/seek_player_bomber.gd
@@ -0,0 +1,88 @@
+extends "res://Libraries/state.gd"
+
+@onready var animation_player = $"../../AnimationPlayer"
+@onready var zombie_groans = $"../../ZombieGroans-Audio"
+@onready var groan_timer = $"../../GroanTimer"
+@onready var distance_check_timer: Timer = $"../../DistanceCheckTimer"
+@onready var spit_detectors = %SpitDetector
+@onready var spit_timer: Timer = $"../../SpitTimer"
+
+@export var groan_interval = 5  # min seconds between random groans
+
+var is_targeting_player = false
+var previous_position: Vector2
+
+func _ready():
+	# Connect to player position signal and feed that to the pathfinding component
+	Events.player_position_change.connect(_on_player_position_changed)
+
+func enter():
+	distance_check_timer.start()
+	previous_position = owner.global_position
+	
+	# Enable spit detectors
+	for rayCast in spit_detectors.get_children():
+		rayCast.enabled = true
+
+	is_targeting_player = true
+	if randi_range(0, 1) == 1:
+		animation_player.play("zombie_walk_basic")
+	else:
+		animation_player.play("zombie_walk_basic_2")
+	groan_timer.start(groan_interval + randf_range(0, 10))
+
+func _on_player_position_changed(player_position: Vector2):
+	# We only consume this signal to update the pathfinding IF we are in this state
+	if is_targeting_player:
+		owner.pathfinding_component.update_target_position(player_position)
+
+# Clean up the state. Reinitialize values like a timer
+func exit():
+	is_targeting_player = false
+	groan_timer.stop()
+	
+	# Disable spit detectors
+	for rayCast in spit_detectors.get_children():
+		rayCast.enabled = false
+
+	return
+
+func update(delta):
+	# Check if we should do a random groan
+	if groan_timer.is_stopped():
+		zombie_groans.play_spitter()
+		groan_timer.start(groan_interval + randf_range(0, 10))
+
+	# Check if theres a player to melee attack
+	if owner.player_detector.has_overlapping_areas():
+		emit_signal("finished", "attack_player")
+		return
+		
+	# Check if the player is in the spit detectors to do a spit attack
+	if spit_timer.is_stopped():
+		for rayCast in spit_detectors.get_children():
+			if rayCast.is_colliding():
+				# 10 second cooldown on spitting
+				spit_timer.start(10)
+				emit_signal("finished" , "attack_player_spit")
+				return
+		
+	# Track the distance we travel to make sure we arent getting stuck
+	if distance_check_timer.is_stopped():
+		var distance_traveled = (owner.global_position - previous_position).length()
+		if distance_traveled <= 50:
+			owner.despawn()
+		else:
+			previous_position = owner.global_position
+			distance_check_timer.start()
+
+	# Move - velocity should be getting updated by the pathfinding component
+	owner.velocity = owner.velocity_component.velocity
+	owner.move_and_slide()
+	
+	# Rotate
+	owner.update_rotation()
+
+func _on_animation_finished(anim_name):
+	return
+
---
+++ Enemies/Bomber/zombie_bomber.gd
@@ -0,0 +1,108 @@
+extends CharacterBody2D
+
+"""
+This should be a lightweight script which glues the different components together.
+
+NOTE: Make sure to add the zombie to the zombie group.
+"""
+
+# Components that need to be added to the rotation function
+@onready var sprite: Sprite2D = $placeholderSprite
+@onready var window_detector: Area2D = $WindowDetector
+@onready var window_hit_box: Area2D = $WindowHitbox
+@onready var player_detector: Area2D = $PlayerDetector
+@onready var player_hit_box: Area2D = $PlayerHitbox
+@onready var trigger_detector: Area2D = $TriggerDetector
+@onready var skeleton_control: Node2D = $SkeletonControl
+@onready var head_blood_emitter: CPUParticles2D = %HeadBloodEmitter
+
+# Helper Nodes
+@onready var pathfinding_component: Node2D = $PathfindingComponent
+@onready var velocity_component: Node = $VelocityComponent
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var state_machine: Node = $ZombieStateMachine
+
+# Health Nodes
+@onready var head_health: Node2D = $"HealthComponents/HealthComponent - Head"
+@onready var left_upper_health: Node2D = $"HealthComponents/HealthComponent - LeftUpper"
+@onready var left_lower_health: Node2D = $"HealthComponents/HealthComponent - LeftLower"
+@onready var right_upper_health: Node2D = $"HealthComponents/HealthComponent - RightUpper"
+@onready var right_lower_health: Node2D = $"HealthComponents/HealthComponent - RightLower"
+
+@onready var modification_stack = preload("res://Enemies/Bomber/zombie_bomber_modification_stack.tres")
+
+var ZOMBIE_DESPAWN_DISTANCE = 4000  # if a zombie gets this far from the player, despawn them
+
+var target_window: Area2D # set by zombie spawner, used by state machine to get zombie through the target window
+
+func _ready():
+	# We must load the modification stack AFTER the zombie is a part of the scene tree or it breaks
+	# We must also duplicate the modification resource or it gets shared between all the zombies (weird but whatever)
+	$SkeletonControl/Skeleton.set_modification_stack(modification_stack.duplicate())
+	$SkeletonControl/Skeleton.get_modification_stack().enabled = true
+	Events.player_position_change.connect(_on_player_position_changed)
+	setWalkingSpeed()
+
+func setWalkingSpeed():
+	randomize()
+	var number = randi_range(70, 200)
+	velocity_component.max_velocity += number
+
+func set_max_health(zombie_base_health: int):
+	"""
+	Called by zombie spawner on spawn.
+	
+	Zombies start with 150 health
+	Zombies health increases by 100 each round until they hit 950 (Round 9)
+	After this, zombie health follows this formula: health = health x 1.1
+	"""
+	head_health.max_health = zombie_base_health * .5
+	head_health.fill_health()
+	left_upper_health.max_health = zombie_base_health * 1.5
+	left_upper_health.fill_health()
+	right_upper_health.max_health = zombie_base_health * 1.5
+	right_upper_health.fill_health()
+	left_lower_health.max_health = zombie_base_health
+	left_lower_health.fill_health()
+	right_lower_health.max_health = zombie_base_health * .5
+	right_lower_health.fill_health()
+
+func _on_player_position_changed(player_position: Vector2):
+	check_despawn(player_position)
+
+func check_despawn(player_position: Vector2):
+	"""
+	Check if the player got too far from the zombie, then despawn the zombie.
+	"""
+	if (player_position - global_position).length() >= ZOMBIE_DESPAWN_DISTANCE:
+		despawn()
+
+func despawn():
+	"""
+	Called also by the move state if a zombie gets stuck.
+	"""
+	Events.emit_signal("zombie_despawn", self)
+	queue_free()
+
+func update_rotation():
+	"""
+	Called by the move state to update the rotation of the zombie.
+	"""
+	var STEER_FORCE = 0.1
+	var angle = velocity_component.velocity.angle()
+	
+	# if the zombies velocity is in the opposite direction as the target position, instead
+	# of rotating towards the velcity, rotate towards the target position.
+	# This was put in because bullet knockbacks were causing zombies to turn towards
+	# the direction they were getting pushed back and it looked bad.
+	if velocity_component.velocity.normalized().dot(pathfinding_component.nagivation_agent.target_position.normalized()) < .2:
+		angle = (pathfinding_component.nagivation_agent.target_position - global_position).angle()
+	
+	sprite.rotation = lerp_angle(sprite.rotation, angle - deg_to_rad(90), STEER_FORCE) 
+	window_detector.rotation = lerp_angle(window_detector.rotation, angle, STEER_FORCE) 
+	window_hit_box.rotation = lerp_angle(window_hit_box.rotation, angle, STEER_FORCE)
+	player_detector.rotation = lerp_angle(player_detector.rotation, angle, STEER_FORCE)
+	player_hit_box.rotation = lerp_angle(player_hit_box.rotation, angle, STEER_FORCE)
+	trigger_detector.rotation = lerp_angle(trigger_detector.rotation, angle, STEER_FORCE)
+	skeleton_control.rotation = lerp_angle(skeleton_control.rotation, angle, STEER_FORCE)
+	head_blood_emitter.rotation = lerp_angle(head_blood_emitter.rotation, angle, STEER_FORCE)
